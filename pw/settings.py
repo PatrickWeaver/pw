a00023d2611c3015449eeee424c59b21d9a45b07
Patrick <pjpweaver@gmail.com>
2013-10-03 17:29:01 -0400

    fix tempaltes with BASE_DIR

--- pw/settings.py
+++ pw/settings.py
@@ -56,6 +56,9 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 ADMIN_MEDIA_PREFIX = S3_URL + 'admin/'
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = '/media/'
@@ -117,8 +120,8 @@ ROOT_URLCONF = 'pw.urls'
 WSGI_APPLICATION = 'pw.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/home/patrick/sites/pw/templates',
-    '/home/patrick/sites/pw/gallery/templates',
+    os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'gallery/templates')
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -189,7 +192,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
