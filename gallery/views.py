141f1b8f5062e73657f27232bd10dc5f4c907a5c
Patrick <pjpweaver@gmail.com>
2013-10-04 15:00:10 -0400

    messing with css

--- gallery/views.py
+++ gallery/views.py
@@ -14,9 +14,21 @@ def projects(request):
 def project(request, project_id):
     project = Project.objects.get(pk=project_id)
     gallery_items = GalleryItem.objects.filter(project=project.id)
-    return render(request, 'project.html', {'project': project, 'gallery_items': gallery_items,})
+    # Single gallery item projects go straight to galery item.
+    if len(gallery_items) == 1:
+        gallery_item = gallery_items[0]
+        media_type = str(gallery_item.media_type)
+        if media_type == "Internet":
+            url = gallery_item.url
+        return render(request, 'gallery_item.html', {'project': project, 'gallery_item': gallery_item, 'media_type': media_type, 'url': url,})
+    # Multiple gallery item or empty projects go to list of gallery items.
+    else:
+        return render(request, 'project.html', {'project': project, 'gallery_items': gallery_items,})
 
 def gallery_item(request, project_id, gallery_item_id):
     project = Project.objects.get(pk=project_id)
     gallery_item = GalleryItem.objects.get(pk=gallery_item_id)
-    return render(request, 'gallery_item.html', {'project': project, 'gallery_item': gallery_item,})
+    media_type = str(gallery_item.media_type)
+    if media_type == "Internet":
+            url = gallery_item.url
+    return render(request, 'gallery_item.html', {'project': project, 'gallery_item': gallery_item, 'media_type': media_type, 'url': url,})
